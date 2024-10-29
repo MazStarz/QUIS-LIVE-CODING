4b5571521d2763020fba40aa8dad0515626e81d7
MazStarz <bintangjunioryongki@gmail.com>
2024-10-29 11:40:56 +0700

    Add files via upload

---
+++ tugas quizz1.cxx
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+	float massa;
+	float energi;
+	float kecepatan;
+	
+int main() {
+	cout << "masukan massa objek(kg): ";
+	cin >> massa;
+	cout << "masukkan kecepatan objek(m/s): ";
+	cin >> kecepatan;
+
+		double hitungEK = 0.5 * massa * kecepatan * kecepatan;
+
+		cout << "energi kinetik: " << hitungEK << "joule" << endl; return 0;	
+
+	
+}
